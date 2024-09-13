d4897469121d6c3f8c807b3b7385ba40fe1d994f
Duck Devr <xiamauth@gmail.com>
2024-09-13 07:53:57 +0700

    Create exp.php

---
+++ exp.php
@@ -0,0 +1,176 @@
+
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>LUCKYSHOP | Expired</title>
+<link rel="icon" href="https://lucky.4hr.shop/assets/img/logo_1.png" type="image/x-icon">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Kanit:wght@600&family=Mitr:wght@300;600&display=swap" rel="stylesheet" />
+<style>
+      * {
+        font-family: "Kanit", sans-serif;
+      }
+  
+      body {
+            background-color: #090909;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-position: center;
+                background-size: cover;
+
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>LUCKYSHOP | Expired</title>
+<link rel="icon" href="https://lucky.4hr.shop/assets/img/logo_1.png" type="image/x-icon">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Kanit:wght@600&family=Mitr:wght@300;600&display=swap" rel="stylesheet" />
+<style>
+      * {
+        font-family: "Kanit", sans-serif;
+      }
+  
+      body {
+            background-color: #090909;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-position: center;
+                background-size: cover;
+                overflow-x: hidden;
+        }
+  
+      .bg-gra {
+        background: linear-gradient(to right,
+       	 Red 20%,
+            OrangeRed 70%,
+            OrangeRed 80%);
+        background: linear-Red(to right,
+            Red(162, 29, 174, 1),
+            Red(54, 28, 239, 1));
+      }
+  
+      @keyframes textShine {
+        0% {
+          background-position: 0% 50%;
+        }
+  
+        100% {
+          background-position: 100% 50%;
+        }
+      }
+  	
+      .fb {
+        font-size:14px;
+        color:#FFFFFF;
+    	}
+    
+      .text-gra {
+        background: linear-gradient(to right,
+            Red 20%,
+            #ffffff 30%,
+            OrangeRed 70%,
+            OrangeRed 80%);
+        -webkit-background-clip: text;
+        background-clip: text;
+        -webkit-text-fill-color: transparent;
+        text-fill-color: transparent;
+        background-size: 500% auto;
+        animation: textShine 5s ease-in-out infinite alternate;
+      }
+    </style>
+</head>
+<body>
+<div class="container-sm">
+<div style="width: fit-content; margin: 0% auto">
+<center>
+<div class="col-3" style="margin-top: 35vh; width: 50vw" data-aos="zoom-out-down" data-aos-duration="2000">
+<script type="text/javascript" style="display:none">
+//<![CDATA[
+window.__mirage2 = {petok:"anl1zOD0QYyOaA2t5NHDTe2iodBTYAuHEXCJ_e0YxwY-1800-0.0.1.1"};
+//]]>
+</script>
+
+<script type="text/javascript" src="https://ajax.cloudflare.com/cdn-cgi/scripts/04b3eb47/cloudflare-static/mirage2.min.js"></script>
+<img class="img-fluid" data-cfsrc="https://lucky.4hr.shop/assets/img/logo_1.png" style="display:none;visibility:hidden;" /><noscript><img src="https://lucky.4hr.shop/assets/img/logo_1.png" class="img-fluid"/></noscript>
+</div>
+<h1 class="text-primary mt-4 text-gra" data-aos="zoom-out-left" data-aos-duration="2500">
+เว็บไซต์หมดอายุการใช้งาน
+</h1>
+
+ <div class="text-white mt-2" data-aos="zoom-out-right" data-aos-duration="2800">
+    <span class="fb">หาก อายุการใช้งานหมดแล้ว ต้องการเช่าเว็บต่อ
+	<br>โปรดต่ออายุภายในระยะเวลา 3 วัน</span>
+ </div>
+<br>
+<div class="text-white-50" data-aos="zoom-out-up" data-aos-duration="2000">
+<a target="_blank" href="https://www.facebook.com/lucky.hr?mibextid=ZbWKwL" class="mt-1 mb-2 btn border-0 bg-gra ps-4 pe-4 text-white">ติดต่อเช่าเว็บ / ต่ออายุ</a>
+<br/>
+</div>
+</center>
+</div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+      AOS.init();
+    </script>
+</body>￼Enter                overflow-x: hidden;
+        }
+  
+      .bg-gra {
+        background: linear-gradient(to right,
+       	 Red 20%,
+            OrangeRed 70%,
+            OrangeRed 80%);
+        background: linear-Red(to right,
+            Red(162, 29, 174, 1),
+            Red(54, 28, 239, 1));
+      }
+  
+      @keyframes textShine {
+        0% {
+          background-position: 0% 50%;
+        }
+  
+        100% {
+          background-position: 100% 50%;
+        }
+      }
+  	
+      .fb {
+        font-size:14px;
+        color:#FFFFFF;
+
+    
+      .text-gra {
+        background: linear-gradient(to right,
+            Red 20%,
+            #ffffff 30%,
+            OrangeRed 70%,
+            OrangeRed 80%);
+        -webkit-background-clip: text;
+        background-clip: text;
+        -webkit-text-fill-color: transparent;
+        text-fill-color: transparent;
+        background-size: 500% auto;
+        animation: textShine 5s ease-in-out infinite alternate;
+      }
+    </style>
+</head>
+<body>
+<div class="container-sm">
+<div style="width: fit-content; margin: 0% auto">
+<center>
+<div class="col-3" style="margin-top: 35vh; width: 50vw" data-aos="zoom-out-down" data-aos-duration="2000">
+<script type="text/javascript" style="display:none">
+//<![CDATA[
+window.__mirage2 = {petok:"anl1zOD0QYyOaA2t5NHDTe2iodBTYAuHEXCJ_e0YxwY-1800-0.0.1.1"};
+//]]>
